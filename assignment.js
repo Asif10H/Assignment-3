0516d0e473a930cff7be3230daa0ca67aca62246
Asif10H <habibullahasif71@gmail.com>
2021-01-22 14:20:02 +0600

    edited some variable name

--- assignment.js
+++ assignment.js
@@ -1,16 +1,16 @@
 //problem-1
 function kilometerToMeter(kilometer){
     if(kilometer < 0){ //If the value of kilometers is negative then this condition will be executed
-        var ans = "kilometer can never be a negative number";
-        return ans;
+        var meter = "kilometer can never be a negative number";
+        return meter;
     }
     else{
-        var ans = kilometer * 1000; // kilometers to meters conversion
-        return ans;
+        var meter = kilometer * 1000; // kilometers to meters conversion
+        return meter;
     }
 }
-
-var kilometer = 89;
+//input
+var kilometer = 10;
 var result = kilometerToMeter(kilometer); // function call
 console.log(result); // output
 
@@ -27,7 +27,7 @@ function budgetCalculator(watch , phone, laptop){
         return totalCost;
     } 
 }
-
+//input
 var watch = 10;
 var phone = 11;
 var laptop = 12;
@@ -37,18 +37,18 @@ console.log(result); // output
 //problem-3 
 function hotelCost(day){
     if(day <= 0){
-        ans = "something wrong";
+        var ans = "something wrong";
         return ans;
     }
-    else if(day>= 1 && day <= 10){ //if the day value is within 1 to 10 then this condition will be executed
+    else if(day >= 1 && day <= 10){ //if the day value is within 1 to 10 then this condition will be executed
         var ans = day * 100; // calculate total cost
         return ans;
     }
     else if(day >= 11 && day <= 20){ //if the day value is within 11 to 20 then this condition will be executed
         var day11To20 = day - 10;
         var day1To10 = day - day11To20;
-        var ans = day1To10 * 100 + day11To20 * 80; // calculate total cost
-        return ans;
+        var totalCost = day1To10 * 100 + day11To20 * 80; // calculate total cost
+        return totalCost;
     }
     else{ //if the value of the day is more than 20 then this condition will be executed
         var day1To10 = 10;
@@ -56,29 +56,29 @@ function hotelCost(day){
         var day11To20 = 10;
         day -= 10;
         var otherDay = day;
-        var ans = day1To10 * 100 + day11To20 * 80 + otherDay * 50; // calculate total cost
-        return ans;
+        var totalCost = day1To10 * 100 + day11To20 * 80 + otherDay * 50; // calculate total cost
+        return totalCost;
     }  
 }
-
+//input
 var day = 10;
  var result = hotelCost(day); // function call
  console.log(result); // output
 
 //problem-4
 function megaFriend(nameList){
-    var ans = 0;
+    var maxLenName;
     var check = 0;
     for(var i = 0; i < nameList.length; i++){
         var sizeOfName = nameList[i].length; // check single string size 
         if(sizeOfName > check){ // max size of string check
-            ans = sizeOfName; // max size of string length store in ans variable
+            maxLenName = nameList[i]; // max size of name store in ans variable
             check = sizeOfName;
         }
     }
-    return ans;
+    return  maxLenName;
 }
-
-var nameOfList = ["hossain", "asif", "tuhin", "salma"];  
+//input
+var nameOfList = ["asif", "hossain", "zia", "mizan", "abdullah", "mahabub"];  
 var result = megaFriend(nameOfList) // function call
 console.log(result); // output
